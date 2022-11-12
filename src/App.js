870c36cff33e7a4072ec4a186fc0786cd4685b81
dc <00danzy@gmail.com>
2022-11-12 22:51:43 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Blogs from './views/Blogs';
+import { CreateBlog } from './views/CreateBlog';
 import EditProfile from './views/EditProfile';
 import Home from './views/Home';
 import Login from './views/Login';
@@ -16,6 +17,7 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/sign-up' element={<Register />} />
           <Route path='/edit-profile' element={<EditProfile />} />
+          <Route path='/create-blog' element={<CreateBlog />} />
           <Route path='/blogs/:id' element={<Blogs />} />
           <Route path='/users/:id' element={<Profile />} />
         </Routes>
