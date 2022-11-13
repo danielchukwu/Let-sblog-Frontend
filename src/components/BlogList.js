f2e75b57151bbec6c635ab50fcafd2ba279306d4
dc <00danzy@gmail.com>
2022-11-13 22:11:32 +0100

    update

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
+import { useUrl } from "../hooks/useUrl";
 
 const BlogCard = ({blogs}) => {
+   const {cloudinary_image_url} = useUrl();
+
    return (
       blogs.map(blog => {
          return (
          <Link to={`/blogs/${blog.id}`} key={blog.id}>
             <div className="blog-card" >
                <div className="blog-img">
-                  <img src={blog.img} alt="blogs" />
+                  <img src={`${cloudinary_image_url}/${blog.img}`} alt="blogs" />
                </div>
                <div className="blog-text">
                   <p className="blog-cat tb-pad-20">{blog.category}</p>
