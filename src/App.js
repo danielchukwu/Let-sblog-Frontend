3d365df35674b64a3fc005000e2b6fd8773c8d41
dc <00danzy@gmail.com>
2022-11-16 22:43:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './views/Login';
 import { ManageBlogs } from './views/ManageBlogs';
 import Profile from './views/Profile';
 import Register from './views/Register';
+import Spinner from './views/Spinner';
 
 
 function App() {
@@ -18,8 +19,10 @@ function App() {
           <Route path='/login' element={<Login />} />
           <Route path='/sign-up' element={<Register />} />
           <Route path='/edit-profile' element={<EditProfile />} />
+          <Route path='/spinner' element={<Spinner />} />
           <Route path='/manage-blogs' element={<ManageBlogs />} />
           <Route path='/create-blog' element={<CreateBlog />} />
+          <Route path='/edit-blog/:id' element={<CreateBlog />} />
           <Route path='/blogs/:id' element={<Blogs />} />
           <Route path='/users/:id' element={<Profile />} />
         </Routes>
