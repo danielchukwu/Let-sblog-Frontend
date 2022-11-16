15b5755da993f993402d4798f034dc25cc050e13
dc <00danzy@gmail.com>
2022-11-16 22:44:01 +0100

    update

--- src/utils/displayPopup.js
+++ src/utils/displayPopup.js
@@ -5,29 +5,31 @@ const displayPopup = (invalid_input=null) => {
    // Messages
    let msg = ''
    if (invalid_input === 'password' || invalid_input === 'short') {
-      msg = 'This password doesn\'t meet requirements!'
+      msg = 'This password doesn\'t meet requirements!';
    } else if (invalid_input === 'username') {
-      msg = 'This Username Is Already Taken!'
+      msg = 'This Username Is Already Taken!';
    } else if (invalid_input === 'invalid_username') {
-      msg = 'This Username Is Invalid!'
+      msg = 'This Username Is Invalid!';
    } else if (invalid_input === 'email'){
-      msg = 'This Email Is Already Taken!'
+      msg = 'This Email Is Already Taken!';
    } else if (invalid_input === 'successful_profile_update' ){
-      msg = 'User Profile Successfully Updated! ✅'
+      msg = 'User Profile Successfully Updated! ✅';
    } else if (invalid_input === 'successful_registration' ){
-      msg = 'Account Was Successfully Created! ✅'
+      msg = 'Account Was Successfully Created! ✅';
    } else if (invalid_input === 'successful_blog_creation' ){
-      msg = 'Blog Was Successfully Created! ✅'
-   } else if (invalid_input === 'successful_registration' ){
-      msg = 'Account Was Successfully Created! ✅'
+      msg = 'Blog Was Successfully Created! ✅';
+   } else if (invalid_input === 'successful_deletion' ){
+      msg = 'Blog Was Successfully Deleted! ✅';
    } else if (invalid_input === 'content_over_5000' ){
-      msg = 'Blog Content should not be more than 5000 characters'
+      msg = 'Blog Content should not be more than 5000 characters';
    } else if (invalid_input === 'content_below_500' ){
-      msg = 'Blog Content should not be less than 500 characters'
-   } 
+      msg = 'Blog Content should not be less than 500 characters';
+   } else {
+      msg = invalid_input;
+   }
    // Template 
    const template = `
-   <div class="pop-up-info">
+   <div class="pop-up-info ppi">
       <div class="pp-card">
          <p style={'textAlign': 'center'}>${invalid_input? msg : 'Invalid Username and Password!'}</p>
       </div>
@@ -41,9 +43,9 @@ const displayPopup = (invalid_input=null) => {
    gsap.from('.pop-up-info', {top: '-100px', duration: 0.3, opacity: 0.2});
    gsap.to  ('.pop-up-info', {top: '-200px', duration: 0.2, opacity: 0.2, delay: 3});
    // Remove template in 3 seconds
-   setTimeout(() => {
-      container.innerHTML = '';
-   }, 3500)
+   // setTimeout(() => {
+   //    container.innerHTML = '';
+   // }, 3500)
 }
 
 export default displayPopup;
