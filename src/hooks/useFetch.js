72f803b171e424d3af34f3c25b6ba492f19b3278
dc <00danzy@gmail.com>
2022-11-16 22:43:57 +0100

    update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -23,7 +23,9 @@ export default function useFetch(url='') {
          .catch(err => {
             setTokenIsValid(false)
             console.log(`Error: ${err.message}`)
-            navigate('/login')
+            if (!getCookie('usrin')){
+               navigate('/login');
+            }
          })
 
       return () => {}
