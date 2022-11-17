ce3f7c091470fe5a76303f89546c90e0dcccae61
dc <00danzy@gmail.com>
2022-11-17 21:49:05 +0100

    update

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -1,9 +1,9 @@
 const FooterMain = () => {   
    return (
-      <footer class="t-pad-100">
-         <div class="footer-wrapper">
-            <div class="owner-container tb-pad-40 max-w-1000">
-               <h3 class="who">Made by Chukwu Daniel 😁</h3>
+      <footer className="t-pad-100">
+         <div className="footer-wrapper">
+            <div className="owner-container tb-pad-40 max-w-1000">
+               <h3 className="who">Made by Chukwu Daniel 😁</h3>
             </div>
          </div>
       </footer>
