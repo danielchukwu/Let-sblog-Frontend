22a96c3c87f9583ba6ff771af2fa11bd08e8887a
dc <00danzy@gmail.com>
2022-11-27 22:58:23 +0100

    update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -10,23 +10,32 @@ export default function useFetch(url='') {
    const location = useLocation()
 
    useEffect(() => {
-      let config = {params : {'x-access-token': getCookie('usrin')}}
-      axios.get(`${process.env.REACT_APP_HOST_API}${url}`, config)
-         .then(data => {
-            // if (data.data.message != 'successful'){
-            //    throw Error('missing token')
-            // }
-            console.log(data.data)
-            setTokenIsValid(true)
-            setData(data.data)
-         })
-         .catch(err => {
-            setTokenIsValid(false)
-            console.log(`Error: ${err.message}`)
-            if (!getCookie('usrin')){
-               navigate('/login');
-            }
-         })
+      let config = {params : {'x-access-token': getCookie('usrin')}};
+      let fetchCount = 0;   // maximum of 5 fetch calls allowed
+      function fetch() {
+         axios.get(`${process.env.REACT_APP_HOST_API}${url}`, config)
+            .then(data => {
+               // if (data.data.message != 'successful'){
+               //    throw Error('missing token')
+               // }
+               console.log(data.data);
+               setTokenIsValid(true);
+               setData(data.data);
+            })
+            .catch(err => {
+               setTokenIsValid(false)
+               console.log(`Error: ${err.message}`)
+               if (!getCookie('usrin')){
+                  navigate('/login');
+               } else {
+                  setTimeout(() => {
+                     fetchCount++;
+                     if (fetchCount < 5) { fetch(); }
+                  }, 10000)
+               }
+            })
+      }
+      fetch()
 
       return () => {}
    }, [url])
