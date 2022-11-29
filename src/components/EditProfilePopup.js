9b16e706fde4a724f1ad1e5372a83fca61ef4be3
dc <00danzy@gmail.com>
2022-11-29 22:56:53 +0100

    update

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -47,22 +47,13 @@ export const EditProfilePopup = ({ setShowEditPopup, owner }) => {
                         </div>
 
                         <div className='ebs t-pad-10'>
-                           
-                           <span>Python <small>&#10006;</small></span>
-                           <span>Javascript <small>&#10006;</small></span>
-                           <span>Typescript <small>&#10006;</small></span>
-                           <span>Flask <small>&#10006;</small></span>
-                           <span>PostgreSql <small>&#10006;</small></span>
-                           <span>Express <small>&#10006;</small></span>
-                           <span>Java <small>&#10006;</small></span>
-                           <span>Flask <small>&#10006;</small></span>
-                           <span>PostgreSql <small>&#10006;</small></span>
-                           <span>Express <small>&#10006;</small></span>
-                           <span>Java <small>&#10006;</small></span>
-                           <span>Flask <small>&#10006;</small></span>
-                           <span>PostgreSql <small>&#10006;</small></span>
-                           <span>Express <small>&#10006;</small></span>
-                           <span>Java <small>&#10006;</small></span>
+                           <span>Python 
+                              <small className='remove-skill'>
+                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                                    <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+                                 </svg>
+                              </small>
+                           </span>
                         </div>
 
                      </div>
