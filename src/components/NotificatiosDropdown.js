681395f349b11a297cff8da51e879fc91179677d
dc <00danzy@gmail.com>
2022-11-29 11:53:42 +0100

    update

--- src/components/NotificatiosDropdown.js
+++ src/components/NotificatiosDropdown.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react'
+import React, { createContext, useState } from 'react'
 import { useUrl } from '../hooks/useUrl'
 import useFetch from '../hooks/useFetch'
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { NotificationList } from './NotificationList';
 
+const NotificationContext = createContext();
 
-export const NotificatiosDropdown = ({ owner }) => {
+
+export const NotificatiosDropdown = ({ owner, setOwner }) => {
    const {data} = useFetch('/users/notifications');
 
    // Notification group states
@@ -17,6 +19,10 @@ export const NotificatiosDropdown = ({ owner }) => {
    const [thisYear, setThisYear] = useState();
    const [old, setOld] = useState();
 
+   // See a particular group state. e.g all the people who like a particular picture
+   const [groupList, setGroupList] = useState();
+
+
    useEffect(() => {
       if (data){
          setToday(data.unseen.today.concat(data.seen.today));
@@ -25,87 +31,93 @@ export const NotificatiosDropdown = ({ owner }) => {
          setThisMonth(data.unseen.this_month.concat(data.seen.this_month));
          setThisYear(data.unseen.this_year.concat(data.seen.this_year));
          setOld(data.unseen.old.concat(data.seen.old));
-         // console.log("Test Case: ")
-         // console.log(...data.seen.today);
+         
+      // set notifications_count to 0
+      const newData = owner;
+      newData.notifications_count = 0;
+      setOwner({...owner});
       }
    }, [data]);
 
-   useEffect(() => {
-      console.log('Yesterday...')
-      if (yesterday){
-         console.log(yesterday);
-      }
-   }, [yesterday])
-
 
    return (
+      <NotificationContext.Provider value={ setGroupList }>
       <div className="pc-wrapper-2">
-         {/* popup Content */}
-         <div className="noti-card">
-            <div className='noti-title'>
-               <h3>Notifications</h3>
-            </div>
-            <div className='noti-body'>
-               <div className='noti-item'>
-                  {/* Today */}
-                  {today && today.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>Today</h3>
-                  </div>}
-                  {/* Today's Notificationa */}
-                  { today && <NotificationList notifications={today} owner={owner} />}
 
-                  {/* Yesterday */}
-                  {yesterday && yesterday.length > 0 && <div className='tb-pad-10'><hr/></div>} 
-                  {yesterday && yesterday.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>Yesterday</h3>
-                  </div>}
-                  {/* Yesterday's Notifications */}
-                  { yesterday && <NotificationList notifications={yesterday} owner={owner} />}
-
-
-                  {/* thisWeek */}
-                  {thisWeek && thisWeek.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>thisWeek</h3>
-                  </div>}
-                  {/* thisWeek's Notifications */}
-                  { thisWeek && <NotificationList notifications={thisWeek} owner={owner} />}
-
-
-                  {/* thisMonth */}
-                  {thisMonth && thisMonth.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>thisMonth</h3>
-                  </div>}
-                  {/* thisMonth's Notifications */}
-                  { thisMonth && <NotificationList notifications={thisMonth} owner={owner} />}
-
-
-                  {/* thisYear */}
-                  {thisYear && thisYear.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>thisYear</h3>
-                  </div>}
-                  {/* thisYear's Notifications */}
-                  { thisYear && <NotificationList notifications={thisYear} owner={owner} />}
-
-
-                  {/* thisYear */}
-                  {thisYear && thisYear.length > 0 && 
-                  <div className='ns-title'>
-                     <h3>thisYear</h3>
-                  </div>}
-                  {/* thisYear's Notifications */}
-                  { thisYear && <NotificationList notifications={thisYear} owner={owner} />}
-                  
+            {/* popup Content */}
+            <div className="noti-card">
+               <div className='noti-title'>
+                  <h3>Notifications</h3>
                </div>
+               <div className='noti-body'>
+                  {/*  */}
+                  { !groupList &&
+                  <div className='noti-item'>
+                     {/* Today */}
+                     {today && today.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>Today</h3>
+                     </div>}
+                     {/* Today's Notificationa */}
+                     { today && <NotificationList notifications={today} owner={owner} />}
 
+                     {/* Yesterday */}
+                     {today && today.length > 0 && <div className='tb-pad-10'><hr/></div>}   {/* section breaker */}
+                     {yesterday && yesterday.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>Yesterday</h3>
+                     </div>}
+                     {/* Yesterday's Notifications */}
+                     { yesterday && <NotificationList notifications={yesterday} owner={owner} />}
+
+
+                     {/* thisWeek */}
+                     {yesterday && yesterday.length > 0 && <div className='tb-pad-10'><hr/></div>}   {/* section breaker */}
+                     {thisWeek && thisWeek.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>This Week</h3>
+                     </div>}
+                     {/* thisWeek's Notifications */}
+                     { thisWeek && <NotificationList notifications={thisWeek} owner={owner} />}
+
+
+                     {/* thisMonth */}
+                     {thisWeek && thisWeek.length > 0 && <div className='tb-pad-10'><hr/></div>}   {/* section breaker */}
+                     {thisMonth && thisMonth.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>This Month</h3>
+                     </div>}
+                     {/* thisMonth's Notifications */}
+                     { thisMonth && <NotificationList notifications={thisMonth} owner={owner} />}
+
+
+                     {/* thisYear */}
+                     {thisMonth && thisMonth.length > 0 && <div className='tb-pad-10'><hr/></div>}   {/* section breaker */}
+                     {thisYear && thisYear.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>This Year</h3>
+                     </div>}
+                     {/* thisYear's Notifications */}
+                     { thisYear && <NotificationList notifications={thisYear} owner={owner} />}
+
+
+                     {/* thisYear */}
+                     {thisYear && thisYear.length > 0 && <div className='tb-pad-10'><hr/></div>}   {/* section breaker */}
+                     {old && old.length > 0 && 
+                     <div className='ns-title'>
+                        <h3>Old</h3>
+                     </div>}
+                     {/* old's Notifications */}
+                     { old && <NotificationList notifications={old} owner={owner} />}
+                     
+                  </div>}
+
+               </div>
+               
             </div>
-            
-         </div>
+
 
       </div>
+      </NotificationContext.Provider>
    )
 }
