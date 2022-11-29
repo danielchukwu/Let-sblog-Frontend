50c338c4db850cc0fe8450d6f5870648a3b2507e
dc <00danzy@gmail.com>
2022-11-29 11:53:28 +0100

    update

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -16,11 +16,12 @@ const CommentList = (props) => {
    // Add Comments
    useEffect(() => {
       setComments(props.comments);
+      // console.log(props.comments)
    }, [props])
 
 
    // Handle Submission
-   const HandleSubmit = async (e, comment_id) => {
+   const HandleSubmit = async (e, comment_id, user_id) => {
       e.preventDefault();
       console.log(`input-${comment_id}`)
       const input = document.querySelector(`#input-${comment_id}`);
@@ -30,7 +31,7 @@ const CommentList = (props) => {
       if (content.length === 0){
          return;
       }
-      const body = {'content': content, 'comment_id': comment_id}
+      const body = {'content': content, 'comment_id': comment_id, 'user_id': user_id}
       // setCommentIsLoading(true);
 
       const updateRepliesCount = () => {
@@ -69,7 +70,7 @@ const CommentList = (props) => {
    
    
    // Handle Like
-   const HandleLike = async (id, is_like, comments, setComments) => {
+   const HandleLike = async (id, is_like, comments, setComments, user_id) => {
       // Passing comments state and the setComments function 
       // will alow the handling of sub comments states
       let config = {params : {'x-access-token': getCookie('usrin')}}
@@ -96,21 +97,42 @@ const CommentList = (props) => {
          setComments([...newDataList])
       }
 
+      // POST like request
+      function sendLikeRequest(like_type) {  // like_type (likes or dislikes)
+         console.log("Sending Like:...")
+         const body = {'user_id': user_id}
+         console.log(body)
+         
+         fetch(`${host_url}/comments/${id}/${like_type}`, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json',
+               'X-Access-Token': getCookie('usrin')
+            },
+            body: JSON.stringify(body)
+         })
+         .then(res => res.json())
+         .then(data => {
+            console.log(data)
+         })
+      }
+
       // Like Or Dislike
       if (is_like){                     // If like. like comment
-         updateLikeStateComments('liked', 'likes', 'disliked', 'dislikes')
-         
-         axios.get(`${process.env.REACT_APP_HOST_API}/comments/${id}/likes`, config)
-         .then((data) => {
-            console.log(data)
-         })
-      } else {                        // If dislike. dislike comment
-         updateLikeStateComments('disliked', 'dislikes', 'liked', 'likes')
+         updateLikeStateComments('liked', 'likes', 'disliked', 'dislikes');
+         sendLikeRequest('likes');
+         // axios.get(`${process.env.REACT_APP_HOST_API}/comments/${id}/likes`, config)
+         // .then((data) => {
+            //    console.log(data)
+            // })
+         } else {                        // If dislike. dislike comment
+            updateLikeStateComments('disliked', 'dislikes', 'liked', 'likes');
+            sendLikeRequest('dislikes');
 
-         axios.get(`${process.env.REACT_APP_HOST_API}/comments/${id}/dislikes`, config)
-         .then((data) => {
-            console.log(data)
-         })
+         // axios.get(`${process.env.REACT_APP_HOST_API}/comments/${id}/dislikes`, config)
+         // .then((data) => {
+         //    console.log(data)
+         // })
       }
 
    }
@@ -171,14 +193,14 @@ const CommentList = (props) => {
                <div className="cb-3 t-pad-5">
                   <span className="ud pointer r-pad-20" onClick={() => {
                         // HandleLike('comments', comment.id, false, comments, setComments)
-                        HandleLike(comment.id, false, comments, setComments)
+                        HandleLike(comment.id, false, comments, setComments, comment.user_id)
                      }}>
                      <svg className="t-pad-5 " style={{fill: comment.disliked ? 'var(--theme-green)' : ''}} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path d="M313.4 479.1c26-5.2 42.9-30.5 37.7-56.5l-2.3-11.4c-5.3-26.7-15.1-52.1-28.8-75.2H464c26.5 0 48-21.5 48-48c0-25.3-19.5-46-44.3-47.9c7.7-8.5 12.3-19.8 12.3-32.1c0-23.4-16.8-42.9-38.9-47.1c4.4-7.3 6.9-15.8 6.9-24.9c0-21.3-13.9-39.4-33.1-45.6c.7-3.3 1.1-6.8 1.1-10.4c0-26.5-21.5-48-48-48H294.5c-19 0-37.5 5.6-53.3 16.1L202.7 73.8C176 91.6 160 121.6 160 153.7V192v48 24.9c0 29.2 13.3 56.7 36 75l7.4 5.9c26.5 21.2 44.6 51 51.2 84.2l2.3 11.4c5.2 26 30.5 42.9 56.5 37.7zM32 320H96c17.7 0 32-14.3 32-32V64c0-17.7-14.3-32-32-32H32C14.3 32 0 46.3 0 64V288c0 17.7 14.3 32 32 32z"/>
                      </svg>
                      <small>{comment.dislikes}</small>
                   </span>
-                  <span className="ud pointer r-pad-20" onClick={() => HandleLike(comment.id, true, comments, setComments)}>
+                  <span className="ud pointer r-pad-20" onClick={() => HandleLike(comment.id, true, comments, setComments, comment.user_id)}>
                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style={{fill: comment.liked ? 'var(--theme-green)' : ''}} >
                         <path d="M313.4 32.9c26 5.2 42.9 30.5 37.7 56.5l-2.3 11.4c-5.3 26.7-15.1 52.1-28.8 75.2H464c26.5 0 48 21.5 48 48c0 25.3-19.5 46-44.3 47.9c7.7 8.5 12.3 19.8 12.3 32.1c0 23.4-16.8 42.9-38.9 47.1c4.4 7.2 6.9 15.8 6.9 24.9c0 21.3-13.9 39.4-33.1 45.6c.7 3.3 1.1 6.8 1.1 10.4c0 26.5-21.5 48-48 48H294.5c-19 0-37.5-5.6-53.3-16.1l-38.5-25.7C176 420.4 160 390.4 160 358.3V320 272 247.1c0-29.2 13.3-56.7 36-75l7.4-5.9c26.5-21.2 44.6-51 51.2-84.2l2.3-11.4c5.2-26 30.5-42.9 56.5-37.7zM32 192H96c17.7 0 32 14.3 32 32V448c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V224c0-17.7 14.3-32 32-32z"/>
                      </svg>
@@ -204,7 +226,7 @@ const CommentList = (props) => {
                            newData.delete(comment.id);
                            setReplyCommentIds(new Set(newData));
                         }}>Cancel</span>
-                        <span class="btn-round l-mar-10" onClick={(e) => HandleSubmit(e, comment.id)}>Reply</span>
+                        <span class="btn-round l-mar-10" onClick={(e) => HandleSubmit(e, comment.id, comment.user_id)}>Reply</span>
                      </div>
                   </div>
                </div>
@@ -219,7 +241,8 @@ const CommentList = (props) => {
                </div>
                <div className="cb-5 t-pad-5">
                   {subComments && subComments[comment.id] &&
-                     <CommentList comments={subComments[comment.id]} setSubComments={setSubComments} HandleLike={HandleLike} />
+                     // <CommentList comments={subComments[comment.id]} setSubComments={setSubComments} HandleLike={HandleLike} />
+                     <CommentList comments={subComments[comment.id]} />
                   }
                </div>
             </div>
