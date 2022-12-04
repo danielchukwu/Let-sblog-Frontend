b2ae57876f103d174253a9ce53ff0aab405c6621
dc <00danzy@gmail.com>
2022-12-04 23:59:05 +0100

    update

--- src/components/UserOptionsDropdown.js
+++ src/components/UserOptionsDropdown.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useUrl } from '../hooks/useUrl'
 
-export const UserOptionsDropdown = ({ owner, handleLogout }) => {
+export const UserOptionsDropdown = ({ owner, handleLogout, setEnableOptionsDropdown }) => {
    const {cloudinary_image_url} = useUrl();
    return (
       <div className="pc-wrapper-2">
-                     
+         
          {/* popup Content */}
          <div className="up-card tb-pad-10">
-            <Link to={`/users/${owner.id}`}>
+            <Link to={`/users/${owner.id}`} onClick={() => setEnableOptionsDropdown(false)}>
                <div className='up-profile'>
                   <div className="round-img-50 r-mar-10">
                      {!owner.avatar && <h3 className="img-text">{owner.name[0].toUpperCase()}</h3>}
@@ -23,7 +23,7 @@ export const UserOptionsDropdown = ({ owner, handleLogout }) => {
                </div>
             </Link>
             <hr />
-            <Link to={`/users/${owner.id}`}><p>Profile</p></Link>
+            <Link to={`/users/${owner.id}`} onClick={() => setEnableOptionsDropdown(false)}><p>Profile</p></Link>
             <Link to={"/create-blog"}><p>Create Blog</p></Link>
             <Link to={"/manage-blogs"}><p>Manage Blogs</p></Link>
             <hr />
