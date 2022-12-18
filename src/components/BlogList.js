aa09baa851d45a840ea25cc8f101931499a5e751
dc <00danzy@gmail.com>
2022-12-18 23:37:44 +0100

    update

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,14 +1,23 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useUrl } from "../hooks/useUrl";
 
 const BlogCard = ({blogs}) => {
    const {cloudinary_image_url} = useUrl();
 
+   // const coverAnimation = (blogCard) => {
+   //    console.log(blogCard)
+   //    console.log(blogCard.getElementsByClassName("blog-img"));
+   //    const blogImg = blogCard.parentElement;
+   //    blogImg.style.cssText = {width: "100px"}
+   // }
+
    return (
       blogs.map(blog => {
          return (
          <Link to={`/blogs/${blog.id}`} key={blog.id}>
-            <div className="blog-card" >
+            <div className="blog-card">
+               {/* <div className="blog-img" onMouseOver={(e) => coverAnimation(e.target)}> */}
                <div className="blog-img">
                   <img src={`${cloudinary_image_url}/${blog.img}`} alt="blogs" />
                </div>
