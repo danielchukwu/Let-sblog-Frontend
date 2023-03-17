7d56d9c6e0610055a2ecc0a171d22a38921c8d17
dc <00danzy@gmail.com>
2023-03-17 10:39:38 +0100

    remove react import

--- src/hooks/useUrl.js
+++ src/hooks/useUrl.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 export const useUrl = () => {
    const host_url = 'http://127.0.0.1:5000/api';
    const cloudinary_image_url = 'http://res.cloudinary.com/dhtcwqsx4/image/upload/v1668247079';
