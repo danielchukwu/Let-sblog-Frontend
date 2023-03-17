7b83eac72829bba24f6e371f29c2c73eab2b7cf9
dc <00danzy@gmail.com>
2023-03-17 10:39:26 +0100

    send secret keys with headers

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,40 +3,43 @@ import { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import getCookie from '../utils/getCookie'
 
-export default function useFetch(url='') {
+export default function useFetch(url='/') {
    const [data, setData] = useState(null)
    const [tokenIsValid, setTokenIsValid] = useState(true)
    const navigate = useNavigate() 
    const location = useLocation()
 
    useEffect(() => {
-      let config = {params : {'x-access-token': getCookie('usrin')}};
+      let config = {
+         headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_HOST_API_KEY}`,
+            'x-access-token': getCookie('usrin')
+         },
+      };
       let fetchCount = 0;   // maximum of 5 fetch calls allowed
-      function fetch() {
+      
+      function fetchData() {
          axios.get(`${process.env.REACT_APP_HOST_API}${url}`, config)
             .then(data => {
-               console.log(url)
-               if (!getCookie('usrin') && url !== '' && url !== '/users/me'){
+               if (!getCookie('usrin') && url !== '/' && url !== '/users/me'){
                   throw Error('missing token')
                }
-               console.log(data.data);
                setTokenIsValid(true);
                setData(data.data);
             })
             .catch(err => {
                setTokenIsValid(false)
-               console.log(`Error: ${err.message}`)
                if (!getCookie('usrin')){
                   navigate('/login');
                } else {
                   setTimeout(() => {
                      fetchCount++;
-                     if (fetchCount < 5) { fetch(); }
+                     if (fetchCount < 5) { fetchData(); }
                   }, 10000)
                }
             })
       }
-      fetch()
+      fetchData()
 
       return () => {}
    }, [url])
