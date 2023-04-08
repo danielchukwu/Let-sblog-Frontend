0c70d067f9d21799010a8ad35bc0c408099f56f3
dc <00danzy@gmail.com>
2023-04-08 21:24:37 +0100

    add mobile footer

--- src/components/FooterMain.js
+++ src/components/FooterMain.js
@@ -1,12 +1,30 @@
+import navItems from "../models/NavItem";
+import DisplaySvg from "./images/DisplaySvg";
+
+
 const FooterMain = () => {   
    return (
-      <footer className="t-pad-100">
-         <div className="footer-wrapper">
-            <div className="owner-container tb-pad-40 max-w-1000">
-               <h3 className="who">Made by Chukwu Daniel 😁</h3>
+      <>
+         {/* Desktop */}
+         <footer className="footer-desktop t-pad-100">
+            <div className="footer-wrapper">
+               <div className="owner-container tb-pad-40 max-w-1000">
+                  <h3 className="who">Made by Chukwu Daniel 😁</h3>
+               </div>
             </div>
-         </div>
-      </footer>
+         </footer>
+
+         {/* Mobile */}
+         <footer className="footer-mobile t-pad-100">
+            <div className="footer-m-wrapper">
+               <div className="footer-m-content">
+                  {navItems.map(navItem => {
+                     return <DisplaySvg svg={navItem.iconSrc} selector={"navbar-svg_selected"} />
+                  })}
+               </div>
+            </div>
+         </footer>
+      </>
    )
 }
 
