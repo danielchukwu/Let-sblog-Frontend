bfabc886394b4aaf8438d4364f77cab6131ec809
dc <00danzy@gmail.com>
2023-04-11 08:54:31 +0100

    remove fit selector from div wrapper

--- src/components/images/DisplayImg.js
+++ src/components/images/DisplayImg.js
@@ -8,7 +8,7 @@ const DisplayImg = ({
 }) => {
   const {cloudinary_image_url} = useUrl();
   return (
-    <div className={selector + " fit"}>
+    <div className={selector}>
       {img && <img className="fit" src={`${cloudinary_image_url}/${img}`} alt={alt} />}
       {!img && name.length > 0 && (
         <p>{name[0].toUpperCase()}</p>
